6b4fd9ea66e73e0151162231a024f4321afd8193
KiRura <57588049+KiRura@users.noreply.github.com>
2025-10-21 00:11:33 +0900

    fix vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,7 @@ import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
 	plugins: [reactRouter(), tsconfigPaths()],
+	server: {
+		allowedHosts: [".kirura.f5.si"],
+	},
 });
